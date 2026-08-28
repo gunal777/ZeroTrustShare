195706aad5405f55b20cb9dd4edd1c2913474664
Gunal <gunalb20@gmail.com>
2026-08-28 18:52:35 +0530

    add route for share

--- backend/src/routes/share.route.js
+++ backend/src/routes/share.route.js
@@ -7,6 +7,8 @@ router.post('/', shareController.createShareLink);
 
 router.get('/:token', shareController.accessSharedFile);
 
+router.post('/:token/access', shareController.verifySharePassword)
+
 router.delete('/:token', shareController.revokeShareLink);
 
 module.exports = router;
