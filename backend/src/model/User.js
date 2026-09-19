a2851680a7590de50e985641f83a1e7213d455b6
Gunal <gunalb20@gmail.com>
2026-09-19 16:33:30 +0530

    updated user model

--- backend/src/model/User.js
+++ backend/src/model/User.js
@@ -4,13 +4,33 @@ const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: true,
-    unique: true
+    required: [true, "Email is required"],
+    unique: true,
+    lowercase: true,
+    trim: true,
+    match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, "Please enter a valid email address"],
   },
   password: {
     type: String,
-    required: true
+    required: [true, "Password is required"],
+    minlength: [6, "Password must be at least 6 characters"],
+    select: false,
+  },
+});
+
+// Pre-save hook: Hash password with bcrypt before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
   }
-})
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 module.exports = mongoose.model('User', userSchema);
